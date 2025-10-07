1b2eab96ac638bc134a59450a437b5dc74af36d6
djangify <djangify@gmail.com>
2025-10-07 17:33:12 +0100

    Update policy pages text and remove any templates not needed

--- core/urls.py
+++ core/urls.py
@@ -6,38 +6,6 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    # cluster pages
-    path(
-        "pdf-creation",
-        TemplateView.as_view(template_name="core/pdf-creation.html"),
-        name="pdf_creation",
-    ),
-    # Project pages
-    path(
-        "projects_index",
-        TemplateView.as_view(template_name="core/projects/projects_index.html"),
-        name="projects_index",
-    ),
-    path(
-        "tracker",
-        TemplateView.as_view(template_name="core/projects/tracker.html"),
-        name="tracker",
-    ),
-    path(
-        "stream",
-        TemplateView.as_view(template_name="core/projects/stream.html"),
-        name="stream",
-    ),
-    path(
-        "posterpalace",
-        TemplateView.as_view(template_name="core/projects/posterpalace.html"),
-        name="posterpalace",
-    ),
-    path(
-        "digital-marketing-specialist",
-        TemplateView.as_view(template_name="core/digital-marketing-specialist.html"),
-        name="digital_marketing_specialist",
-    ),
     # policies
     path("policy/privacy/", views.privacy_view, name="privacy_policy"),
     path("policy/cookies/", views.cookie_view, name="cookie_policy"),
